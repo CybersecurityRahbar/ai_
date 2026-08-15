a37041d7ae9f9b712c62caed81fa6608adeecc50
CybersecurityRahbar <alghfryloberalghwly@gmail.com>
2026-08-15 21:47:16 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,15 @@ plugins {
 
 android {
     namespace = "com.example.personalmemoryai"
+
     compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.personalmemoryai"
+
         minSdk = 23
         targetSdk = 35
+
         versionCode = 1
         versionName = "0.1"
     }
@@ -31,12 +34,17 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.18.0")
-    implementation("androidx.activity:activity-ktx:1.13.0")
-    implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.android.material:material:1.13.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.10.2")
+    // AndroidX
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+
+    // Material
+    implementation("com.google.android.material:material:1.11.0")
+
+    // Kotlin Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
 
     // Local OCR
     implementation("com.google.mlkit:text-recognition:16.0.1")
