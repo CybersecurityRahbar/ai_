165d7125162ae22211d0a9acfc30adbd04796969
CybersecurityRahbar <alghfryloberalghwly@gmail.com>
2026-08-15 23:24:08 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
 
     implementation("com.google.android.material:material:1.11.0")
 
