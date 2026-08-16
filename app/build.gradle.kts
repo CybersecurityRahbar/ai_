cfb3b159d3f3d81f2c28534d08b22c4f5381512d
CybersecurityRahbar <alghfryloberalghwly@gmail.com>
2026-08-17 00:15:30 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.example.personalmemoryai"
 
-        minSdk = 23
+        minSdk = 24
         targetSdk = 35
 
         versionCode = 2
